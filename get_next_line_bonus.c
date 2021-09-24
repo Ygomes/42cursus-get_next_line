b89e56a28a42cf0c995a6ef553e985d2e74b93b7
Yuri <yurigoliveir4@gmail.com>
2021-09-24 18:41:24 -0300

    finalização

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,21 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygomes-d <ygomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 08:44:55 by ygomes-d          #+#    #+#             */
-/*   Updated: 2021/09/24 12:17:26 by ygomes-d         ###   ########.fr       */
+/*   Updated: 2021/09/24 18:35:16 by ygomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-static char	*make_next_str(char *str, int i)
+static char	*make_next_str(char *str)
 {
 	char	*next_str;
+	int		i;
 
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
 	next_str = ft_strdup(str + i);
 	free (str);
 	return (next_str);
@@ -48,10 +54,16 @@ static char	*make_line(int fd, char *str, char *buff)
 	return (str);
 }
 
-static char	*cpyline(char *str, int i)
+static char	*cpyline(char *str)
 {
 	char	*line;
+	int		i;
 
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
 	line = ft_substr(str, 0, i);
 	if (!line[0])
 	{
@@ -63,28 +75,19 @@ static char	*cpyline(char *str, int i)
 
 char	*get_next_line(int fd)
 {
-	static char	*str;
+	static char	*str[MAX_DEFAULT];
 	char		*buff;
 	char		*line;
-	int			i;
 
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || fd > MAX_DEFAULT)
 		return (NULL);
 	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (!buff)
-	{
-		free (buff);
 		return (NULL);
-	}
-	str = make_line(fd, str, buff);
-	if (!str)
+	str[fd] = make_line(fd, str[fd], buff);
+	if (!str[fd])
 		return (NULL);
-	while (str[i] && str[i] != '\n')
-		i++;
-	if (str[i] == '\n')
-		i++;
-	line = cpyline(str, i);
-	str = make_next_str(str, i);
+	line = cpyline(str[fd]);
+	str[fd] = make_next_str(str[fd]);
 	return (line);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,20 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ygomes-d <ygomes-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/16 08:44:52 by ygomes-d          #+#    #+#             */
-/*   Updated: 2021/09/24 11:37:14 by ygomes-d         ###   ########.fr       */
+/*   Updated: 2021/09/24 18:36:18 by ygomes-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <unistd.h>
 # include <stdlib.h>
+
+# define MAX_DEFAULT 256
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
